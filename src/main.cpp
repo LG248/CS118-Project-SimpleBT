4fe0af0be994266243abb1337d96bb1758dfd936
LG <lg@cs-212-115.cs.ucla.edu>
2015-02-12 15:23:13 -0800

    .

--- src/main.cpp
+++ src/main.cpp
@@ -125,8 +125,8 @@ main(int argc, char** argv)
     int colonPos = announce.find(':', hostStartPos);
     int slashPos = announce.find('/', hostStartPos);
     
-    std::string trackerHost = announce.substr(hostStartPos, 17);
-    std::string trackerPortStr = announce.substr(colonPos+1, 23);
+    std::string trackerHost = announce.substr(hostStartPos, 17 - 8);
+    std::string trackerPortStr = announce.substr(colonPos+1, 23 - 8);
     //std::cout << "announceAfter: " << announceAfter << std::endl;
     std::cout << "hostStartPos, colonPos, slashPos: " << hostStartPos << ", " << colonPos << ", " << slashPos << std::endl;
     std::cout << "host and port: " << trackerHost << ", " << trackerPortStr << std:: endl;
